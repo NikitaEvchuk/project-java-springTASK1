5a52b1ea8c9bd50b8c61228624568a13380e411e
NikitaEvchuk <evchuknikita29@gmail.com>
2025-12-23 21:39:05 +0200

    Fixed commit

--- project-java-API/src/main/java/FirstRestApiSpring/product/support/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package FirstRestApiSpring.product.support.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(Long id) {
-        super(String.format("Product with %d not found", id));
-    }
-}
